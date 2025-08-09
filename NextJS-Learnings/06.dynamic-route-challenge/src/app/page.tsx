038009320009d860dce5294de5103c7a121b127b
Vighnesh-Gaddam <vgnshgdm@gmail.com>
2025-08-10 00:59:54 +0530

    added dynamic route challenge

---
+++ NextJS-Learnings/06.dynamic-route-challenge/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center h-screen text-center">
+      <h1 className="text-4xl font-bold mb-4">Welcome to the Home Page</h1>
+      <p className="text-lg mb-8">Search url as http://localhost:3000/users/1 or http://localhost:3000/users/2 it will show user details.</p>
+    </main>
+  );
+}
