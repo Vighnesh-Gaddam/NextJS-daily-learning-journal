58635021034af96c3c5cef8f79c09d395d1eb268
Vighnesh-Gaddam <vgnshgdm@gmail.com>
2025-08-26 20:22:50 +0530

    added post method learning

---
+++ NextJS-Learnings/12.backend-http-methods/src/app/db.ts
@@ -0,0 +1,6 @@
+// db.ts
+export const books = [
+  { id: 1, title: "Atomic Habits", author: "James Clear" },
+  { id: 2, title: "The Pragmatic Programmer", author: "Andrew Hunt" },
+  { id: 3, title: "Clean Code", author: "Robert C. Martin" },
+];
---
+++ NextJS-Learnings/12.backend-http-methods/src/app/page.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link"
+
+const Home = () => {
+  return (
+    <div className="flex justify-center items-center h-screen flex-col">
+      <h1>Http Methods</h1>
+      <div className="flex gap-4">
+        <Link href="/books" className="mt-4 underline text-blue-600">get-books</Link>
+        {/* <Link href="/post" className="mt-4 underline text-blue-600"> POST</Link>
+        <Link href="/put" className="mt-4 underline text-blue-600"> PUT</Link>
+        <Link href="/delete" className="mt-4 underline text-blue-600"> DELETE</Link> */}
+      </div>
+    </div>
+  )
+}
+export default Home
