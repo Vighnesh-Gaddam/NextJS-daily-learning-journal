9a9c9469904ef13f3e7fdae22cfee33062f04909
Vighnesh-Gaddam <vgnshgdm@gmail.com>
2025-09-05 23:48:16 +0530

    added project files in 24.2.music player

---
+++ Next-Projects/24.2.music-player/src/app/musics/layout.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react"
+
+const MusicLayout = ({children,sidebar}:{children: ReactNode,sidebar: ReactNode}) => {
+  return (
+    <div className="flex">
+        {sidebar}
+        <div></div>
+        {children}
+    </div>
+  )
+}
+export default MusicLayout
